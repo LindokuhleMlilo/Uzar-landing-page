bbefdac17ab8918f38712b0350e551d0dc0230a1
lindokuhlemlilo <lindokuhlemlilo89@gmail.com>
2025-03-11 14:03:27 +0200

    comments

--- uzar/src/components/Footer.jsx
+++ uzar/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="w-full border-t py-6 md:py-0 bg-gradient-to-r from-blue-50 to-purple-50">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="flex items-center gap-2">
-          {/* Fixed: Changed <Image> to <img> */}
+         
           <img
             src="./src/assets/images/universal.png"
             alt="Universal Protocol Logo"
